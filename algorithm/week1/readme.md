3111d5b58ab3a706cb447c7da490f99f3d62b4a0
신대한 <71871927+qlkdkd@users.noreply.github.com>
2024-04-15 20:31:42 +0900

    Delete algorithm/week1 directory

--- algorithm/week1/readme.md
+++
@@ -1,2 +0,0 @@
-* [chap1](https://github.com/qlkdkd/univ-3-1/blob/main/algorithm/week1/chap1.md)
-* [chap2](https://github.com/qlkdkd/univ-3-1/blob/main/algorithm/week1/chap2.md)
--- algorithm/week1/w1_10_팩토리얼_계산.py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    if n==1:
-        return 1
-    else:
-        return n*factorial(n-1)
-    
-if __name__=='__main__':
-    n=int(input("숫자 입력: "))
-    print("입력한 숫자의 팩토리얼 값: ", factorial(n))
